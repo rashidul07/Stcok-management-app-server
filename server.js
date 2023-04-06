51e2974e87f1a2bf5aae63c4eb5eb0d7a27339a8
rashidulkarim <rashidul.karim7@gmail.com>
2023-04-06 21:04:04 +0600

    Optimize some rest api

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const corsOptions = {
   credentials: true,
 }
 //remove cors for development
-app.use(cors(corsOptions))
-// app.use(cors({
-//   origin: 'http://localhost:5173',
-// }));
+//app.use(cors(corsOptions))
+app.use(cors({
+  origin: 'http://localhost:5173',
+}));
 app.use(express.json())
 
 
@@ -35,117 +35,69 @@ const run = async () => {
     const database = client.db(process.env.DB_NAME)
     const collection = database.collection("products")
     const stockCollection = database.collection("stockProducts")
-    const deletedProduct = database.collection("deletedProducts")
-    
-    //get all product from DB
+
+    //get all product from DB ** working
     app.get("/products", async (req, res) => {
       const result = await collection.find({}).sort({ "company": 1 }).toArray()
       res.send(result)
     })
 
-    //get all Stock product from DB
-    app.get("/stockProducts", async (req, res) => {
-      const result = await stockCollection.find({}).sort({ "company": 1 }).toArray()
-      setTimeout(() => {
-        res.send(result)
-      }, 10000)
-    })
-
-    //add products
+    //add products to DB ** working
     app.post('/addProducts', async (req, res) => {
-      const product = req.body.productsCollection
-      const existingProduct = product.filter(pd => pd._id)
-
-      const existingProductData = existingProduct.map(pd => {
-        return {
-          updateOne: {
-            filter: { _id: ObjectId(pd._id) },
-            update: { $set: { name: pd.name, label: pd.label, quantity: pd.quantity, company: pd.company, updated_at: pd.updated_at, updated_by: pd.updated_by } },
-          }
+      const products = req.body.productsCollection
+     
+      let modifiedCount = 0;
+      let insertedCount = 0;
+      
+      for (const pd of products) {
+        const filter = { _id: ObjectId(pd._id) };
+        const options = { upsert: true };
+        if (pd.hasOwnProperty('_id')) {
+          delete pd._id;
         }
-      })
-
-      const newProduct = product.filter(pd => !pd._id)
-      let response = {}
-      const promises = [];
-
-      if (newProduct.length > 0) {
-        promises.push(collection.insertMany(newProduct))
+        const update = { $set: pd }
+        const result = await collection.updateOne(filter, update, options);
+        
+        if (result.upsertedCount > 0) {
+          insertedCount++;
+        } else if (result.modifiedCount > 0) {
+          modifiedCount++;
+        }
       }
-
-      if (existingProductData.length > 0) {
-        promises.push(collection.bulkWrite(existingProductData))
-      }
-
-      Promise.all(promises).then((results) => {
-        results.forEach((result) => {
-          if (result.insertedCount) {
-            response.insertedCount = result.insertedCount;
-          }
-          if (result?.result?.nModified) {
-            response.modifiedCount = result.result.nModified;
-          }
-        });
-
-        res.send(response);
-
-      }).catch((error) => {
-        res.status(500).send('Error occurred while processing the request');
-      });
+      res.send({ modifiedCount, insertedCount });
     })
 
-    //add many stockProducts
-    app.post('/addStockProduct', async (req, res) => {
-      const product = req.body.productsCollection
-      const existingProduct = product.filter(pd => pd._id)
-
-      const existingProductData = existingProduct.map(pd => {
-        return {
-          updateOne: {
-            filter: { _id: ObjectId(pd._id) },
-            update: { $set: { name: pd.name, 
-              label: pd.label, 
-              quantity: pd.quantity, 
-              company: pd.company, 
-              price: pd.price, 
-              invoiceDiscount: pd.insertedCount,
-              extraDiscount : pd.extraDiscount, 
-              updated_at: pd.updated_at, 
-              updated_by: pd.updated_by } },
+       //delete many product ** need to change the url && working
+       app.delete('/product', async (req, res) => {
+        const query = req.body
+        const data = query.map(pd => {
+          return ObjectId(pd._id)
+        })
+  
+        const result = await collection.deleteMany({ _id: { $in: data } })
+        res.send(result)
+      })
+  
+  
+      //update many items ** need to change the url && working
+      app.put('/update', async (req, res) => {
+        const query = req.body
+  
+        const data = query.map(pd => {
+          return {
+            updateOne: {
+              filter: { _id: ObjectId(pd._id) },
+              update: { $set: { status: pd.status } }
+            }
           }
-        }
+        })
+  
+        const result = await collection.bulkWrite(data)
+        res.send(result)
+  
       })
 
-      const newProduct = product.filter(pd => !pd._id)
-      let response = {}
-      const promises = [];
-
-      if (newProduct.length > 0) {
-        promises.push(stockCollection.insertMany(newProduct))
-      }
-
-      if (existingProductData.length > 0) {
-        promises.push(stockCollection.bulkWrite(existingProductData))
-      }
-
-      Promise.all(promises).then((results) => {
-        results.forEach((result) => {
-          if (result.insertedCount) {
-            response.insertedCount = result.insertedCount;
-          }
-          if (result?.result?.nModified) {
-            response.modifiedCount = result.result.nModified;
-          }
-        });
-
-        res.send(response);
-
-      }).catch((error) => {
-        res.status(500).send('Error occurred while processing the request');
-      });
-    })
-
-    //delete a product
+    //delete a product ** i think did't need this
     app.delete('/product/:id', async (req, res) => {
       const query = req.params.id
       const id = { _id: ObjectId(query) }
@@ -153,38 +105,8 @@ const run = async () => {
       res.send(result)
     })
 
-    //delete many product
-    app.delete('/product', async (req, res) => {
-      const query = req.body
-      const data = query.map(pd => {
-        return ObjectId(pd._id)
-      })
-
-      const result = await collection.deleteMany({ _id: { $in: data } })
-      res.send(result)
-    })
-
-
-    //update many items
-    app.put('/update', async (req, res) => {
-      const query = req.body
-
-      const data = query.map(pd => {
-        return {
-          updateOne: {
-            filter: { _id: ObjectId(pd._id) },
-            update: { $set: { status: pd.status } }
-          }
-        }
-      })
-
-      const result = await collection.bulkWrite(data)
-      res.send(result)
-
-    })
-
-    //update status
-    app.put('/product/:id', async (req, res) => {
+     //update status ** i think did't need this
+     app.put('/product/:id', async (req, res) => {
       const params = req.params.id
       const query = req.query.status
 
@@ -198,7 +120,7 @@ const run = async () => {
       res.send(result)
     })
 
-    //update quantity status
+    //update quantity status ** i think did't need this
     app.put('/productQuantity/:id', async (req, res) => {
       const params = req.params.id
       const query = req.query.quantity
@@ -212,7 +134,26 @@ const run = async () => {
       res.send(result)
     })
 
-    //find by company name
+    //update product info ** i think did't need this
+    app.put('/productUpdate', async (req, res) => {
+      const pd = req.body
+      const productId = pd._id
+      const id = { _id: ObjectId(productId) }
+      const updateDoc = {
+        $set: {
+          name: pd.name,
+          label: pd.label,
+          quantity: pd.quantity,
+          company: pd.company,
+          updated_at: pd.updated_at,
+          updated_by: pd.updated_by
+        },
+      };
+      const result = await collection.updateOne(id, updateDoc)
+      res.send(result)
+    })
+
+    //find by company name ** i think did't need this
     app.get('/products/:company', async (req, res) => {
       const query = req.params.company
       const company = { company: (query) }
@@ -220,7 +161,39 @@ const run = async () => {
       res.send(result)
 
     })
-    
+
+    /*------- all stock product api --------*/
+
+    //get all Stock product from DB ** working
+    app.get("/stockProducts", async (req, res) => {
+      const result = await stockCollection.find({}).sort({ "company": 1 }).toArray()
+        res.send(result)
+    })
+
+    //add many stockProducts ** working
+    app.post('/addStockProduct', async (req, res) => {
+      const products = req.body.productsCollection
+      let modifiedCount = 0;
+      let insertedCount = 0;
+      
+      for (const pd of products) {
+        const filter = { _id: ObjectId(pd._id) };
+        const options = { upsert: true };
+        if (pd.hasOwnProperty('_id')) {
+          delete pd._id;
+        }
+        const update = { $set: pd }
+        const result = await stockCollection.updateOne(filter, update, options);
+        
+        if (result.upsertedCount > 0) {
+          insertedCount++;
+        } else if (result.modifiedCount > 0) {
+          modifiedCount++;
+        }
+      }
+      res.send({ modifiedCount, insertedCount });
+    })
+
   } catch (error) {
     console.log(error);
     process.exit(1)
